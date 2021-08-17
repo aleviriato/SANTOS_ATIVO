317a5a0856543b0a354b6ba368bf9760fc30118e
Alexandre Viriato <aleviriato@gmail.com>
2021-08-17 11:42:24 -0300

    Task 59327 - Inserção de Script par ainserção do tipo Sexta Parte na tb_codigo

---
+++ Task/Task59327/1-script.sql
@@ -0,0 +1,8 @@
+/*********************
+Data HMG : 17/08/2021 11:37 
+USU: AVIRIATO
+Data PRD : 
+USU:
+/*********************/
+Insert into tb_codigo (COD_INS,COD_NUM,COD_PAR,DES_DESCRICAO,DES_DESCRICAO_CURTA,NUM_ORDEM,COD_COR_SIPREV,DAT_ING,DAT_ULT_ATU,NOM_USU_ULT_ATU,NOM_PRO_ULT_ATU,COD_SONDA,FLG_VIGENTE,FLG_AUTOATEND,COD_COR_ESOCIAL) 
+values ('0','10097','9','Sexta Parte','Sexta Parte','9',null,sysdate,sysdate,'TSILVA','MANUAL',null,'S','N',null);
